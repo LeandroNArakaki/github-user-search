7c48ca9ccddaa40968bb20b63b14b1bde9527487
Leandro Arakaki <leanarakaki@gmail.com>
2020-11-26 15:47:16 -0400

    Criado o componente navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+const Navbar = () => (
+    <nav className="header">
+        <div className="title">
+            <a href="link" className="nav-logo-text"> 
+                <h4>Bootcamp DevSuperior</h4>
+            </a>
+        </div>
+    </nav>
+)
+
+export default Navbar;
