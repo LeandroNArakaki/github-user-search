f7d1d237e8996901717c3a432fc97232cdecdaa1
Leandro Arakaki <leanarakaki@gmail.com>
2020-12-01 19:37:05 -0400

    Criado a chamada de requisição de dados do Github

---
+++ src/core/pages/Search/components/BaseForm/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+
+const BaseForm = () => {
+    return(
+        <div></div>
+    );
+}
+
+export default BaseForm;
--- src/core/pages/Search/index.tsx
+++ src/core/pages/Search/index.tsx
@@ -1,21 +1,47 @@
 import ButtonForm from 'core/components/ButtonForm';
-import React from 'react';
+import { makeRequest } from 'core/utils/request';
+import React, { useState } from 'react';
 import './styles.scss';
 
-const Search = () => (
-    <form className="search-container" >
-        <div className="search-content">
-            <h1 className="search-text">Encontre um perfil Github</h1>
-            <div className="search-input-container">
-                <input type="text" className="search-input" placeholder="Usuário Github" />
+
+type FormState = {
+    name:string;
+}
+
+const Search = () => {
+
+    const [formData, setFormData] = useState<FormState>({
+        name:''
+    });
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        
+        setFormData(data => ({ ...data, [name]: value }) );
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        makeRequest( { url:`users/${formData.name}`} );
+    }
+    return (
+        <form className="search-container" onSubmit={handleSubmit} >
+            <div className="search-content">
+                <h1 className="search-text">Encontre um perfil Github</h1>
+                <div className="search-input-container">
+                    <input type="text" className="search-input" placeholder="Usuário Github" 
+                        name="name" value={formData.name}  onChange={handleOnChange} />
+                </div>
+                <ButtonForm text="Encontrar" />
             </div>
-            <ButtonForm text="Encontrar" />
-        </div>
-        <div className="search-found-container">
+            <div className="search-found-container">
+                <div className="search-found-content">
 
-        </div>
-
-    </form>
-);
+                </div>
+            </div>
+        </form>
+    );
+};
 
 export default Search;
